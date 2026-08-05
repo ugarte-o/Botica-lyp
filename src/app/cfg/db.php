27bf27fd6b9496c7229fb623ba2d70b9d3911fa8
ugarte <obeduga123@gmail.com>
2026-08-05 16:02:59 -0500

    Descripción de los cambios

---
+++ src/app/cfg/db.php
@@ -0,0 +1,9 @@
+<?php
+
+$data = array(
+    "host" => "127.0.0.1",
+    "db"   => "botica",
+    "user" => "root",
+    "pass" => "",
+    "port" => "3306",
+);
---
+++ src/app/cfg/sysmail.php
@@ -0,0 +1,19 @@
+<?php
+$data=array(
+	"auth"=>array(
+		"Host"=>"novoingenios.com",
+		"SMTPAuth"=>true,
+		"Username"=>"noreply@novoingenios.com",
+		"From"=>"noreply@novoingenios.com",
+		"Sender"=>"noreply@novoingenios.com",
+		"Password"=>"",
+		"Port"=>"587",
+		"useSMTPssl"=>false,
+		"Helo"=>"",
+		"FromName"=>"Meralda",
+		
+	)
+);
+
+
+?>
